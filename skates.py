b14ef4348351611a8d9328910da89c5028c382ce
DC Smith <45472555+CodeSmith92@users.noreply.github.com>
2021-07-20 02:57:31 -0400

    Update skates.py

--- skates.py
+++ skates.py
@@ -7,8 +7,9 @@ local_path = os.path.dirname(os.path.abspath(__file__))
 # CLI arguments
 parser = argparse.ArgumentParser(description='Command line arguments for skate size and model suggestions.')
 
-parser.add_argument('--shoe_size', type=float, choices=[6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12],
-                    help='US M shoe size. Only accepted inputs are sizes 6-12, in increments of 0.5.',
+parser.add_argument('--shoe_size', type=float,
+                    choices=[6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13],
+                    help='US M shoe size. Only accepted inputs are sizes 6-13, in increments of 0.5.',
                     required=True)
 parser.add_argument('--foot_width', type=float, help='Widest part of foot in centimeters (trace foot w/socks on).',
                     required=True)
@@ -20,11 +21,17 @@ parser.add_argument('--foot_length', type=float, help='Optional input. Foot leng
 args = parser.parse_args()
 
 
-def sizetoLength():
+def sizeToLength():
+    """Estimates foot length based on shoe size."""
+
     shoe_size = args.shoe_size
 
     # Length is in units of cm
-    if shoe_size == 12:
+    if shoe_size == 13.0:
+        foot_length = 30
+    elif shoe_size == 12.5:
+        foot_length = 29.6
+    elif shoe_size == 12:
         foot_length = 29.1
     elif shoe_size == 11.5:
         foot_length = 28.7
@@ -68,7 +75,7 @@ def getWidthRatio():
         return width_ratio
 
     else:
-        foot_length = sizetoLength()
+        foot_length = sizeToLength()
         width_ratio = foot_length / foot_width
 
         return width_ratio
@@ -76,18 +83,18 @@ def getWidthRatio():
 
 def getSkateVolume():
     width_ratio = getWidthRatio()
-    print(f'length-to-width ratio: {round(width_ratio, 2)}')
+    print(f'length-to-width ratio: {round(width_ratio, 3)}')
 
-    if width_ratio >= 3.1:
+    if width_ratio > 3.05:
         volume = 'low'
-    elif width_ratio >= 2.9:
+    elif width_ratio >= 2.95:
         volume = 'low-medium'
-    elif width_ratio >= 2.6:
+    elif width_ratio > 2.55:
         volume = 'medium'
-    elif width_ratio >= 2.4:
+    elif width_ratio >= 2.45:
         volume = 'medium-high'
     else:
-        volume = 'higher'
+        volume = 'high'
 
     print(f'Suggested skate volume based on length-to-width ratio: {volume}')
     return volume
@@ -96,7 +103,7 @@ def getSkateVolume():
 def main():
     width = args.foot_width
     if width <= 0 | width >= 20:
-        raise argparse.ArgumentTypeError(f"{width} is an invalid (human) foot width")
+        raise argparse.ArgumentTypeError(f'{width} is an invalid (human) foot width')
     return width
 
     shoe_size = args.shoe_size
@@ -106,25 +113,36 @@ def main():
     vol = getSkateVolume()
 
     print('Skate fit/model suggestions:')
-    if vol == 'lower-volume':
-        data = [('CCM', 'JetSpeed', 'D', 'low-medium'), ('Bauer', 'Vapor', 'D', 'low')]
 
-        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Skate Volume'])
+    if vol == 'low':
+        data = [('Bauer', 'Vapor', 'D', 'tapered'),
+                ('CCM', 'Ribcor', 'D', 'tapered/flexible')]
+
+        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Fit Profile'])
         print(df)
+    if vol == 'low-medium':
+        data = [('CCM', 'JetSpeed', 'D', 'tapered'),
+                ('CCM', 'Ribcor', 'D', 'tapered/flexible'), ('Bauer', 'Vapor', 'EE', 'tapered')]
 
-    elif vol == 'medium-volume':
-        data = [('CCM', 'Super Tacks', 'D', 'medium'), ('CCM', 'Tacks', 'D', 'medium'),
-                ('CCM', 'JetSpeed', 'EE', 'medium'), ('Bauer', 'Supreme', 'D',
-                                                      'medium')]
-
-        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Skate Volume'])
+        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Fit Profile'])
         print(df)
+    elif vol == 'medium':
+        data = [('CCM', 'Tacks/Super Tacks', 'D', 'standard/medium'),
+                ('CCM', 'JetSpeed', 'EE', 'tapered'),
+                ('Bauer', 'Supreme', 'D', 'standard/medium')]
 
+        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Fit Profile'])
+        print(df)
+    elif vol == 'medium-high':
+        data = [('CCM', 'Tacks/Super Tacks', 'EE', 'standard/medium'),
+                ('Bauer', 'Supreme', 'EE', 'standard/medium'),
+                ('Bauer', 'Nexus', 'D', 'wide')]
+        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Fit Profile'])
+        print(df)
     else:
-        data = [('CCM', 'Super Tacks', 'EE', 'medium-high'), ('CCM', 'RBZ', 'D', 'high'), ('Bauer', 'Nexus', 'D',
-                                                                                           'high')]
-
-        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Skate Volume'])
+        data = [('CCM', 'RBZ', 'D', 'wide'), ('CCM', 'RBZ', 'EE', 'wide'), ('Bauer', 'Nexus', 'D', 'wide'),
+                ('Bauer', 'Nexus', 'EE', 'wide')]
+        df = pd.DataFrame(data, columns=['Manufacturer', 'Skate Model', 'Width', 'Fit Profile'])
         print(df)
         print('NOTE: CCM recommends sizing down 2 sizes from shoe size for the RBZ skate line.')
         print(f'RBZ skate size: {shoe_size - 2.0}')
